9a09e207a1e7afdce707f21a69c94fd584229deb
sachiyuki <thousandsoaraway03@i.softbank.jp>
2018-11-16 21:48:07 +0900

    20181116

--- TastProject01/src/Helloworld.java
+++ TastProject01/src/Helloworld.java
@@ -4,11 +4,12 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-for(int i=1;i<=5;i++){
-	System.out.println("★");
-}
-		
-		
+int i=10;
+
+do{
+	System.out.println(i);
+	i-=3;
+}while(i>=1);
 }
 
 }

ebb0cbd4f26486c7aa8cca036ad606efb738c85c
ShoheiMiyashiro123 <velonica0301@gmail.com>
2018-11-19 21:42:34 +0900

    20181119

--- human/src/jp/co/internous/action/Human.java
+++ human/src/jp/co/internous/action/Human.java
@@ -9,6 +9,9 @@ public class Human {
 		
 		HumanAge efg=new HumanAge();
 		System.out.println(efg.getAge());
+		
+		HumanAddress hij=new HumanAddress();
+		System.out.println(hij.getAddress());
 	}
 	
 }

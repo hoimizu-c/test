fc19bd71a13e038d755c102e26977a9f89ef52d4
horimizu-c <thousandsoaraway03@i.softbank.jp>
2018-11-28 21:49:07 +0900

    20181128

---
+++ testDB/src/testDB/DBConnector.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package testDB;
+
+/**
+ * @author testuser
+ *
+ */
+public class DBConnector {
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+	import java.sql.Connection;
+	
+	
+	/**
+	 * <p>MySQLに接続する為のユーリティクラスです。<br>
+	 * ルートアカウントにてDBに接続されます。</p>
+	 */
+	public class DBConnector{
+	
+	/**
+	 * JDBC ドライバー名
+	 */
+	private static String driverName="com.mysql.jdbc.Driver";
+	/**
+	 * データベース接続URL
+	 */
+	private static String url=
+"jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+	
+	
+	/**
+	 * データベース接続ユーザ名
+	 */
+	private static String password="root";
+	/**
+	 * データベース接続パスワード
+	 */
+	private static String password="mysql";
+	
+	
+	
+public Connection getConnection(){
+	Connection con=null;
+	try{
+	Class.forName(driverName);
+	con=DriverManager.getConnection(url,user,password);
+	}catch(ClassNotFoundException e){
+		e.printStackTrace();
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+	return con;
+}
+		
+	}
+
+
